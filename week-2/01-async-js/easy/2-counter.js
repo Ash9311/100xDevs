23e47a3c15b8ac65e17f99df405e60566c82ec7c
@Ash9311 <ashutoshmulki7@gmail.com>
2023-12-11 23:41:19 +0530

    week 1,2

---
+++ week-2/01-async-js/easy/2-counter.js
@@ -0,0 +1,84 @@
+// ## Counter without setInterval
+
+// Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+
+let i =0;
+let limit = 10;
+for(let j=0;j<limit;j++){
+    function solve(j){
+        setTimeout(()=>{
+            console.log(j);
+        },1000*j)
+    }
+    solve(j)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (Hint: setTimeout)
