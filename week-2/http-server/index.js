5075c5c604162b5cd171bcca89f4e9e62cc98282
@Ash9311 <ashutoshmulki7@gmail.com>
2023-12-16 23:50:19 +0530

    write file and week 3 init

--- week-2/http-server/index.js
+++ week-2/http-server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-     console.log(req.body);
+    console.log(req.body);
     res.send('Hello !');
 })
 
@@ -17,6 +17,14 @@ app.get('/', (req, res) => {
 app.get('/route-handler', (req, res) => {
     res.json({ name: "ash", age: 25 });
 })
+
+app.use(express.json())
+//kidneys = [1,2]
+app.post("/ ", function (req, res) {
+    const kidneys = req.body.kidneys;
+    const kidneyLength = kidneys.length;
+    res.send("you have " + kidneyLength + " kidneys");
+});
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
