281ce3e30bd01af9c718f5b13015836b9f14ecb6
@Ash9311 <ashutoshmulki7@gmail.com>
2023-12-25 13:36:43 +0530

    week-3- JWT

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
-
+const zod = require('zod');
 
 /**
  * Generates a JWT for a given username and password.
@@ -13,8 +13,19 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
 function signJwt(username, password) {
     // Your code here
+    const usernamereponse = emailSchema.safeParse(username);
+    const passwordresponse = passwordSchema.safeParse(password);
+    if (!usernamereponse.success || !passwordresponse.success) {
+        return null;
+    }
+    const signature = jwt.sign({ username }, jwtPassword);
+    return signature;
 }
 
 /**
@@ -27,6 +38,13 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+    let ans = true;
+    try {
+        jwt.verify(token, "secret");
+    } catch (error) {
+        ans = false;
+    }
+    return ans;
 }
 
 /**
@@ -38,12 +56,17 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+    const decoded = jwt.decode(token);
+    if (decoded) {
+        return true;
+    }
+    return false;
 }
 
 
 module.exports = {
-  signJwt,
-  verifyJwt,
-  decodeJwt,
-  jwtPassword,
+    signJwt,
+    verifyJwt,
+    decodeJwt,
+    jwtPassword,
 };
