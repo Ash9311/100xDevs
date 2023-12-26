9de2a67e9f26252bbe92a58245ae1f8ae3e096fc
@Ash9311 <ashutoshmulki7@gmail.com>
2023-12-26 23:21:15 +0530

    week-3- course-site

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,20 +1,67 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User } = require("../db");
+
+const usernameExists = async (username) => {
+    const foundUser = await User.findOne({ username });
+    return foundUser ? true : false;
+}
 
 // User Routes
-app.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    if (await usernameExists(username)) {
+        res.status(403).json({ message: 'username already exists' })
+    }
+    else {
+        const user = new User({ username, password });
+        const savedUser = user.save();
+        res.status(201).json({ message: 'User created successfully' })
+    }
 });
 
-app.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+    const courses = await Course.find({});
+    if (!courses) {
+        res.status(404).json({ message: 'not found' });
+        return;
+    }
+    else {
+        return res.status(200).json({ 'courses': courses });
+    }
 });
 
-app.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
+    const courseId = req.params.courseId;
+    const course = await Course.findById(courseId);
+    if (!course) {
+        res.status(404).json({ message: `course ${courseId} not found` });
+        return;
+    }
+    const user = req.username;
+    if (user.purchasedCourses.includes(courseId)) {
+        return res.status(409).json({ message: "course already purchased" });
+    }
+    const purchasedCourse = {
+        id: courseId, title: 'course title',
+        description: 'course description', price: 100,
+        imageLink: 'https://linktoimage.com', published: true
+    }
+    user.purchasedCourses.push(purchasedCourse);
+    return res.status(200).json({ message: "Course purhcased successfully" });
+
 });
 
-app.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic
+    const user = req.username;
+    if (!user.purchasedCourses) {
+        return req.status(404).json({ message: "user has no course purchased" });
+    }
+    return res.status(200).json({ 'purchasedCourses': user.purchasedCourses })
 });
