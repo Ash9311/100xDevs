5e0a935c41c210605551b2cc153d52cb829db196
@Ash9311 <ashutoshmulki7@gmail.com>
2023-12-29 01:16:31 +0530

    week-3- mongo with jwt init-sigin,signup

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,21 +1,41 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, User } = require("../db");
 const router = Router();
-
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = require('../config');
 // Admin Routes
-app.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    let username = req.body.username;
+    let password = req.body.password;
+    await Admin.create({
+        username: username,
+        password: password
+    })
+    return res.status(201).json({ message: 'User created successfully' })
 });
 
-app.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await User.find({ username, password });
+    if (user) {
+        const token = jwt.sign({ username }, JWT_SECRET);
+        res.json({ token });
+    }
+    else {
+        res.status(411).json({ message: "Incorrect email/password" })
+    }
 });
 
-app.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
 });
 
-app.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
 });
 
