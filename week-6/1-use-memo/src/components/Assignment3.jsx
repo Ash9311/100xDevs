f113415e86359975fa63d3c7de660e8da711c155
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-01-22 13:15:34 +0530

    week-6 assignments

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -11,9 +11,24 @@ const Assignment3 = () => {
         // Add more items as needed
     ]);
 
+
+
+    function calculateCost(items) {
+        let total = 0;
+        for (let i = 0; i < items.length; i++) {
+            total += items[i].value;
+        }
+        return total;
+    }
+
     // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
+    const totalValue = useMemo(() => {
+        // return calculateCost(items);
+        let total = 0;
+        items.map(item => total += item.value);
+        return total;
+    }, [items]);
+    //Your code ends here
     return (
         <div>
             <ul>
