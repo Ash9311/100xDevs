68c4c2427a0ce7d0a7ed4071d98d347df27f22a4
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-01-23 00:11:54 +0530

    week-6 assignments

--- week-6/2-use-callback/src/components/Assignment2.jsx
+++ week-6/2-use-callback/src/components/Assignment2.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useCallback } from 'react';
+import { useMemo } from 'react';
+import { useEffect } from 'react';
 
 // Create a component with a text input field and a button. The goal is to display an alert with the text entered when the button is clicked. Use useCallback to memoize the event handler function that triggers the alert, ensuring it's not recreated on every render.
 // Currently we only have inputText as a state variable and hence you might not see the benefits of 
@@ -6,13 +8,18 @@ import React, { useState, useCallback } from 'react';
 
 export function Assignment2() {
     const [inputText, setInputText] = useState('');
+    const [inputText2, setInputText2] = useState('');
 
     // Your code starts here
-    function showAlert() {
 
-    }
+
+    const showAlert = useCallback(() => {
+        alert(inputText);
+    }, [inputText])
     // Your code ends here
-
+    const showAlert2 = useCallback(() => {
+        alert(inputText2);
+    }, [inputText2])
     return (
         <div>
             <input
@@ -21,12 +28,23 @@ export function Assignment2() {
                 onChange={(e) => setInputText(e.target.value)}
                 placeholder="Enter some text"
             />
+            <input
+                type="text2"
+                value={inputText2}
+                onChange={(e) => setInputText2(e.target.value)}
+                placeholder="Enter some text 2"
+            />
             <Alert showAlert={showAlert} />
+            <Alert2 showAlert2={showAlert2} />
         </div>
     );
 };
 
-function Alert({showAlert}) {
+function Alert({ showAlert }) {
     return <button onClick={showAlert}>Show Alert</button>
+}
+
+function Alert2({ showAlert2 }) {
+    return <button onClick={showAlert2}>Show Alert 2</button>
 }
 
