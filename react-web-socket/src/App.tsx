4c583a8781949a3929c4f5647d055c331c38e7c0
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-04-07 22:25:57 +0530

    websockets & redis

---
+++ react-web-socket/src/App.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function useSocket() {
+  const [socket, setSocket] = useState<null | WebSocket>(null)
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080');
+    socket.onopen = () => {
+      console.log('Connected');
+      setSocket(socket);
+    }
+    return () => {
+      socket.close();
+    }
+  }, []);
+  return socket;
+}
+
+
+function App() {
+  const socket = useSocket();
+
+  const [latestMessage, setLatestMessage] = useState("");
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    if(socket){
+    socket.onmessage = (message) => {
+      console.log('Received message:', message.data);
+      setLatestMessage(message.data);
+    }
+  }
+  }, [])
+
+if (!socket) {
+  return (
+    <div>
+      Loading...
+    </div>
+  )
+}
+
+return (
+  <>
+    <input onChange={(e) => { setMessage(e.target.value) }}></input>
+    <button onClick={() => { socket.send(message); }}>
+      Send
+    </button>
+    {latestMessage}
+  </>
+)
+}
+
+export default App
