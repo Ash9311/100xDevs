c68abcb5b43fa5740c6bbfa83a653941cc6d08cc
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-05-05 19:56:29 +0530

    web-rtc

--- rl-ddos-cap/Backend/dist/index.js
+++ rl-ddos-cap/Backend/dist/index.js
@@ -7,28 +7,26 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.use(express_1.default.json());
-// Store OTPs in a simple in-memory object
 const otpStore = {};
-// Endpoint to generate and log OTP
 app.post('/generate-otp', (req, res) => {
     const email = req.body.email;
     if (!email) {
         return res.status(400).json({ message: "Email is required" });
     }
-    const otp = Math.floor(100000 + Math.random() * 900000).toString(); // generates a 6-digit OTP
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
     otpStore[email] = otp;
-    console.log(`OTP for ${email}: ${otp}`); // Log the OTP to the console
+    console.log(`OTP for ${email} is ${otp}`);
     res.status(200).json({ message: "OTP generated and logged" });
 });
-// Endpoint to reset password
+//reset password
 app.post('/reset-password', (req, res) => {
     const { email, otp, newPassword } = req.body;
     if (!email || !otp || !newPassword) {
-        return res.status(400).json({ message: "Email, OTP, and new password are required" });
+        return res.status(400).json({ message: "Email,OTP and new password is required" });
     }
-    if (otpStore[email] === otp) {
-        console.log(`Password for ${email} has been reset to: ${newPassword}`);
-        delete otpStore[email]; // Clear the OTP after use
+    if (otpStore[email] == otp) {
+        console.log(`Password for ${email} is reset to ${newPassword}`);
+        delete otpStore[email]; //del after use
         res.status(200).json({ message: "Password has been reset successfully" });
     }
     else {
@@ -36,5 +34,5 @@ app.post('/reset-password', (req, res) => {
     }
 });
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`server running on PORT ${PORT}`);
 });
---
+++ web-rtc/frontend/src/components/Receiver.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react"
+
+export function Receiver() {
+return (
+    <>
+        
+    </>
+)
+}
